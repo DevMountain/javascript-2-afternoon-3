f79f6dd88461bc1fa3865718a9749ced4a7189f2
Tyler McGinnis <tylermcginnis33@gmail.com>
2014-08-22 14:24:03 -0600

    First commit

---
+++ callbackPractice.js
@@ -0,0 +1,62 @@
+/* Declare and Define the functions here that will make the function calls below work properly */
+
+
+
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+first(names, function(firstName){
+  console.log('The first name in names is ', firstName)
+});
+
+last(names, function(lastName){
+  console.log('The last name in names is ', lastName);
+});
+
+contains('Colt', function(yes){
+  if(yes){
+    'Colt is in the array';
+  } else {
+    'Colt is not in the list';
+  }
+});
+
+uniq(names, function(uniqArr){
+  console.log('The new names array with all the duplicate items removed is ', uniqArr);
+});
+
+each(names, function(item, indice){
+  console.log('The item in the ' + indice + 'position is ' + item)
+});
+
+var users = [
+  {
+    id: '12d',
+    email: 'tyler@gmail.com',
+    name: 'Tyler',
+    address: '167 East 500 North'
+  },
+  {
+    id: '15a',
+    email: 'cahlan@gmail.com',
+    name: 'Cahlan',
+    address: '135 East 320 North'
+  },
+  {
+    id: '16t',
+    email: 'ryan@gmail.com',
+    name: 'Ryan',
+    address: '192 East 32 North'
+  },
+];
+
+getUserById('16t', function(user){
+  console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address); 
+});
+
+
+
+
+
+
+
