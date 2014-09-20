527454f08dbb1c1841a31834ad5cf4ee03c3a140
Tyler McGinnis <tylermcginnis33@gmail.com>
2014-09-20 07:49:24 -0600

    Reformat Questions

--- callbackPractice.js
+++ callbackPractice.js
@@ -1,36 +1,133 @@
-/* Declare and Define the functions here that will make the function calls below work properly */
+/* In this repo your job is to write functions to make each function call work properly.
+Below is a sample problem 
+
+  //code here for favNum
+
+   var favoriteNumber = 13;
+   favNum(favoriteNumber, function(){
+      console.log('My favorite number is ', + 13);
+   });
+   
+
+and what you should write is the favNum function that makes the code above work, 
+    
+    
+   var favNum = function(num, cb){
+    cb(num);
+   }
+
+   var favoriteNumber = 13;
+   favNum(favoriteNumber, function(){
+      console.log('My favorite number is ', + 13);
+   });
+    
+    
+*/
 
 
 
+  //Code Here for first
+  
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
-
 first(names, function(firstName){
   console.log('The first name in names is ', firstName)
 });
 
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+  //Code Here for last
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 last(names, function(lastName){
   console.log('The last name in names is ', lastName);
 });
 
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+
+  //Code Here for multiply
+
 multiply(4, 3, function(answer){
-  console.log('The answer is ', answer);
+  console.log('The answer is ', answer); //should console.log 12
 })
 
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+  //Code Here for contains
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 contains(names, 'Colt', function(yes){
   if(yes === true){
     'Colt is in the array';
   } else {
-    'Colt is not in the list';
+    'Colt is not in the array';
   }
 });
 
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+    //Code Here for uniq
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
 });
 
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+    //Code Here for each
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
   console.log('The item in the ' + indice + 'position is ' + item)
 });
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+ //code here for getUserById
 
 var users = [
   {
@@ -56,10 +153,3 @@ var users = [
 getUserById('16t', function(user){
   console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address); 
 });
-
-
-
-
-
-
-
